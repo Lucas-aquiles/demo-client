ce389185bb74f76edb5c51f67acbaf2bc33aed10
lucas <you@example.com>
2022-04-27 01:21:48 -0300

    chan

--- src/Components/Home/Home1.js
+++ src/Components/Home/Home1.js
@@ -15,12 +15,12 @@ import { faArrowRightRotate, faQuestion } from '@fortawesome/free-solid-svg-icon
 const Home1 = () => {
 
     const allCountries = useSelector((state) => state.countries)
-
+    const activities = useSelector((state) => state.activities)
     const dispatch = useDispatch()
 
-    // useEffect(() => {
-    //     dispatch(getCountries())
-    // }, [allCountries])
+    useEffect(() => {
+        dispatch(getCountries())
+    }, [activities])
     // dispatch(clearStateCountries())
 
 
